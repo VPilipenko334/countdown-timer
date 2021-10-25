0c266fb236305ae6dfb8b6300e2aeb1578d41164
Veronika <veronika.pilipenko@gmail.com>
2021-10-25 19:44:59 -0400

    easy timer

--- timer/src/App.js
+++ timer/src/App.js
@@ -2,22 +2,19 @@ import React from 'react';
 import { render } from 'react-dom';
 import './App.css';
 
-class Timer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function App() {
+  const [counter, setCounter] = React.useState(100);
 
-  render() {
-      return (
-      <div className="App">
-        <header className="App-header">
-          
-          hi 
-          "Hello this is working"
-        </header>
-      </div>
-    );
-  }
+  React.useEffect(() => {
+    const timer =
+      counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+    return () => clearInterval(timer);
+  }, [counter]);
+
+  return (
+    <div className="App">
+      <div>Countdown: {counter}</div>
+    </div>
+  );
 }
-
-export default Timer;
+export default App;
